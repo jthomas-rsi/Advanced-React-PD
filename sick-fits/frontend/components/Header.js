61287399e5e9363346570ec40c750210e5f8f76d
James F. Thomas <jthomas@rsimail.com>
2024-07-06 18:41:52 -0500

    frontend - new components Header & Nav with styled components

---
+++ sick-fits/frontend/components/Header.js
@@ -0,0 +1,50 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+import Nav from './Nav';
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  background: red;
+  transform: skew(-7deg);
+  a {
+    padding: 0.5rem 1rem;
+    background: ${(props) => props.theme.red};
+    color: white;
+    text-transform: uppercase;
+    text-decoration: none;
+  }
+`;
+
+const HeaderStyles = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: stretch;
+  }
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid var(--black, black);
+  }
+`;
+
+const Header = () => (
+  <HeaderStyles>
+    <div className="bar">
+      <Logo>
+        <Link href="/">Sick Fits</Link>
+      </Logo>
+    </div>
+    <div className="sub-bar">
+      <p>Search</p>
+    </div>
+    <Nav />
+  </HeaderStyles>
+);
+
+export default Header;
--- sick-fits/frontend/components/PageComponent.js
+++ sick-fits/frontend/components/PageComponent.js
@@ -1,7 +1,9 @@
 import PropTypes from 'prop-types';
+import Header from './Header';
 
 const PageComponent = ({ children }) => (
   <div>
+    <Header />
     <h1>Page Component</h1>
     {children}
   </div>

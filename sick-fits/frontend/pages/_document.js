594e319b943f5559dcd138259c0913b9c0b766bf
James F. Thomas <jthomas@rsimail.com>
2024-07-17 11:13:19 -0500

    reverted _document to class component correcting error

--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -1,15 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 // Custom Document to globally add custom html, head and body tags to the application
 
-const Document = () => {
-  <Html lang="en-US">
-    <Head />
-    <body>
-      <Main />
-      <NextScript />
-    </body>
-  </Html>;
-};
-
-export default Document;
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en-US">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}

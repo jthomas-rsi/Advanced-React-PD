f7da98f37f61ab823d3f4d02c0f1c89016963863
James F. Thomas <jthomas@rsimail.com>
2024-07-18 19:01:40 -0500

    implemented custom useForm hook in sell form

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,7 +1,12 @@
 import { useState } from 'react';
+import useForm from '../lib/useForm';
 
 const CreateProduct = () => {
-  const [name, setName] = useState('');
+  const { inputs, handleChange } = useForm({
+    name: 'Cool James',
+    price: 12345,
+    description: 'This is a really cool guy',
+  });
 
   return (
     <form>
@@ -12,8 +17,30 @@ const CreateProduct = () => {
           id="name"
           name="name"
           placeholder="Name"
-          onChange={(e) => setName(e.target.value)}
-          value={name}
+          onChange={handleChange}
+          value={inputs.name}
+        />
+      </label>
+      <label htmlFor="price">
+        Price
+        <input
+          type="text"
+          id="price"
+          name="price"
+          placeholder="price"
+          value={inputs.price}
+          onChange={handleChange}
+        />
+      </label>
+      <label htmlFor="description">
+        Description
+        <input
+          type="text"
+          id="description"
+          name="description"
+          placeholder="Description"
+          onChange={handleChange}
+          value={inputs.description}
         />
       </label>
     </form>

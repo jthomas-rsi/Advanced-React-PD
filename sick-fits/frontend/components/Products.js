e97029166c1ff46bf0feac5da3de61c526d574bd
James F. Thomas <jthomas@rsimail.com>
2024-07-31 19:14:15 -0500

    updated ALL_PRODUCTS_QUERY to accept skip and first variables

--- sick-fits/frontend/components/Products.js
+++ sick-fits/frontend/components/Products.js
@@ -1,14 +1,22 @@
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 import Product from './Product';
 import DisplayError from './ErrorMessage';
+import { perPage } from '../config';
+
+const ProductsListStyles = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+`;
 
 // create graphql query string for fetching products
 // TODO create separate folder with files for all queries and mutations
 export const ALL_PRODUCTS_QUERY = gql`
-  query ALL_PRODUCTS_QUERY {
-    allProducts {
+  query ALL_PRODUCTS_QUERY($skip: Int = 0, $first: Int) {
+    allProducts(first: $first, skip: $skip) {
       id
       name
       price
@@ -23,17 +31,14 @@ export const ALL_PRODUCTS_QUERY = gql`
   }
 `;
 
-const ProductsListStyles = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-gap: 60px;
-`;
-
-const Products = () => {
+const Products = (page) => {
   // useQuery hook to fetch data from the server
-  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
-
-  // console.log('query data', data, error, loading);
+  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY, {
+    variables: {
+      skip: page * perPage - perPage,
+      first: perPage,
+    },
+  });
 
   // create conditional rendering for loading and error states
   if (loading) <>Loading ...</>;
@@ -49,6 +54,9 @@ const Products = () => {
       </ProductsListStyles>
     </div>
   );
+};
+Products.propType = {
+  page: PropTypes.number,
 };
 
 export default Products;

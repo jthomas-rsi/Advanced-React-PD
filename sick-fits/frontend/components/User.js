8b0ba89909afa8a4f1ed3bb48bcc6a0d49701db6
James F. Thomas <jthomas@rsimail.com>
2024-08-01 16:05:21 -0500

    useUser custom hook, current_user query, authenticated user links, signin page

---
+++ sick-fits/frontend/components/User.js
@@ -0,0 +1,34 @@
+import { gql, useQuery } from '@apollo/client';
+
+export const CURRENT_USER_QUERY = gql`
+  query GetUser {
+    authenticatedItem {
+      ... on User {
+        id
+        email
+        name
+        # TODO: Query the cart once we have it
+        # cart {
+        #   id
+        #   quantity
+        #   product {
+        #     id
+        #     price
+        #     name
+        #     description
+        #     photo {
+        #       image {
+        #         publicUrlTransformed
+        #       }
+        #     }
+        #   }
+        # }
+      }
+    }
+  }
+`;
+
+export const useUser = () => {
+  const { data } = useQuery(CURRENT_USER_QUERY);
+  return data?.authenticatedItem;
+};
---
+++ sick-fits/frontend/pages/signin.js
@@ -0,0 +1,3 @@
+const SignInPage = () => <div>Welcome to the Sign In Page!</div>;
+
+export default SignInPage;

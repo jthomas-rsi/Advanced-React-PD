4e0d82659d619959bd7b9235f3ffbb63d5617e86
James F. Thomas <jthomas@rsimail.com>
2024-08-16 12:28:07 -0500

    created AddToCart btn - implemented in Product component - ADD_TO_USER mutation - button adding items to cart as desired

--- sick-fits/frontend/components/Product.js
+++ sick-fits/frontend/components/Product.js
@@ -5,32 +5,36 @@ import Title from './styles/Title';
 import PriceTag from './styles/PriceTag';
 import formatMoney from '../lib/formatMoney';
 import DeleteProduct from './DeleteProduct';
+import AddToCart from './AddToCart';
 
-const Product = ({ productData }) => (
-  // console.log({ productData });
-  <ItemStyles>
-    <img
-      src={productData?.photo?.image?.publicUrlTransformed}
-      alt={productData.name}
-    />
-    <Title>
-      <Link href={`/product/${productData.id}`}>{productData.name}</Link>
-    </Title>
-    <PriceTag>{formatMoney(productData.price)}</PriceTag>
-    <p>{productData.description}</p>
-    <div className="buttonList">
-      <Link
-        href={{
-          pathname: '/update',
-          query: { id: productData.id },
-        }}
-      >
-        Edit ✏️
-      </Link>
-      <DeleteProduct id={productData.id}>Delete</DeleteProduct>
-    </div>
-  </ItemStyles>
-);
+const Product = ({ productData }) => {
+  console.log({ productData });
+  return (
+    <ItemStyles>
+      <img
+        src={productData?.photo?.image?.publicUrlTransformed}
+        alt={productData.name}
+      />
+      <Title>
+        <Link href={`/product/${productData.id}`}>{productData.name}</Link>
+      </Title>
+      <PriceTag>{formatMoney(productData.price)}</PriceTag>
+      <p>{productData.description}</p>
+      <div className="buttonList">
+        <Link
+          href={{
+            pathname: '/update',
+            query: { id: productData.id },
+          }}
+        >
+          Edit ✏️
+        </Link>
+        <AddToCart id={productData.id} />
+        <DeleteProduct id={productData.id}>Delete</DeleteProduct>
+      </div>
+    </ItemStyles>
+  );
+};
 Product.propTypes = {
   productData: PropTypes.object.isRequired,
 };
